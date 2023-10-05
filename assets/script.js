5cb424b2861e38770d470b2346b009cae83dd8db
Jacob Kueck <jacob.kueck@gmail.com>
2023-10-04 20:13:04 -0500

    A rough start of javascript, and a review of previous javascript done in class.

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,8 @@
 var generateBtn = document.querySelector("#generate");
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var numbers = "1234567890"
+var extras = "!@#$%^&*()_+=/?"
 
 // Write password to the #password input
 function writePassword() {
