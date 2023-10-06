3ffb44d9e46d64c3f272fd72ba13d71a96d508e0
Jacob Kueck <jacob.kueck@gmail.com>
2023-10-05 22:27:27 -0500

    Readme finished and javascript failed.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Password-Gen
-Password Generater 
+
+## Description
+- This is an unfinished password generator.
+- i wanted to show and prove the improvements in my coding.
+- though determination and hard work I will never have a assignment come out this bad again.
+- I have realized javascript if going to be harder than I though
+
+## Usage 
+Use this how ever you see fit as it does not work. Maybe try to fix my mistakes. if I ever have the time I will come back and redo this assignment.
+
+## Credits 
+N/A
+
+## Screen shots
+It is basically no different from the starter code.
--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,28 @@
 var generateBtn = document.querySelector("#generate");
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var lowerCase = "abcdefghijklmnopqrstuvwxyz"
-var numbers = "1234567890"
-var extras = "!@#$%^&*()_+=/?"
+var weirdNumbers = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "1234567890";
+var extras = "!@#$%^&*()_+=/?";
+// variables created for creating password
+var passwordText = document.getElementById("password");
+var length = document.getElementById("length");
+var Numb = document.getElementById("numbers");
+var Symb = document.getElementById("generate");
+// tells you what your password will consist of
+var tagP = alert("characters must be between 8 and 128, with one number, one upper case, one lower case, and one symbol");
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+
+
+
+  generateBtn.addEventListener("click");
+  
+}
+
